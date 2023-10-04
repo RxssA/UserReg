5583a50b3c841aee0c1ccff88595e7673cfb98b3
G00401655@atu.ie <G00401655@atu.ie>
2023-10-04 14:44:21 +0100

    push no.1

---
+++ src/main/java/ie/atu/userreg/EmailService.java
@@ -0,0 +1,7 @@
+package ie.atu.userreg;
+import org.springframework.stereotype.Service;
+public class EmailService {
+    public void sendEmail(String to, String message){
+
+    }
+}
---
+++ src/main/java/ie/atu/userreg/UserController.java
@@ -0,0 +1,9 @@
+package ie.atu.userreg;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserController {
+
+}
