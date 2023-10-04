5ea2e5076597d8ac4f826dd5ecaeac9f7c486500
G00401655@atu.ie <G00401655@atu.ie>
2023-10-04 16:00:06 +0100

    prints username and password from body

--- src/main/java/ie/atu/userreg/UserController.java
+++ src/main/java/ie/atu/userreg/UserController.java
@@ -1,15 +1,16 @@
 package ie.atu.userreg;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
     private UserService us;
+    private UserStorage userStorage;
+
     @Autowired
     public void UserService(UserService userService){
         this.us = userService;
@@ -20,4 +21,12 @@ public class UserController {
 
         return us.registerUser(name, email);
     }
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserStorage registerUserCredential(@RequestBody UserStorage userStorage){
+        System.out.println("User ID: "+ userStorage.getUserName());
+        System.out.println("User ID: "+ userStorage.getPassword());
+        return userStorage;
+    }
+
 }
---
+++ src/main/java/ie/atu/userreg/UserStorage.java
@@ -0,0 +1,17 @@
+package ie.atu.userreg;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserStorage {
+    private String userName;
+    private String password;
+
+
+
+}
