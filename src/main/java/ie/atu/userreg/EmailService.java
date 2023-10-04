13a0bc5040933b1d61d491c2f126b66a6126dd94
G00401655@atu.ie <G00401655@atu.ie>
2023-10-04 15:13:29 +0100

    prints email q1 done

--- src/main/java/ie/atu/userreg/EmailService.java
+++ src/main/java/ie/atu/userreg/EmailService.java
@@ -3,6 +3,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
     public String sendEmail(String to, String message){
-        return " email "+ to;
+        return "email "+ to;
     }
 }
