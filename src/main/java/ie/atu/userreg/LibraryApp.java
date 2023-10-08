e9bf773396ba8e2ad5058a4ced8fe2d3800e1168
RxssA <g00401655@atu.ie>
2023-10-08 15:27:20 +0100

    added getBooks & addBooks, setters and getters, library controller

---
+++ src/main/java/ie/atu/userreg/LibraryApp.java
@@ -0,0 +1,10 @@
+package ie.atu.userreg;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class LibraryApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApp.class, args);
+    }
+}
